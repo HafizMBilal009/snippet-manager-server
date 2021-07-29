84ff60cf8f6d9be179cc54ea41f5ed9a98cab78b
HafizMBilal009 <bilal@datumbrain.com>
2021-07-29 12:21:20 +0500

    Squashed commit of the following:
    commit 2624aa553fbccff4c21e400c1d9adf1e53078cd6
    Author: HafizMBilal009 <bilal@datumbrain.com>
    Date:   Thu Jul 29 12:16:28 2021 +0500
    
        Completed All Routes & Authentication
    
    commit fc66252b5e90860e96b9de5078a3f44bb39d3ea1
    Author: HafizMBilal009 <bilal@datumbrain.com>
    Date:   Mon Jul 26 17:00:02 2021 +0500
    
        Added CRUD Routes

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const snippetRoutes = require('./routes/snippet');
+const userRoutes = require('./routes/user');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use('/snippet', snippetRoutes);
+app.use('/auth', userRoutes);
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@snippet-manager.cnbee.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error) => {
+    if (error) return console.log({ error });
+    console.log('Connected to MongoDB');
+  }
+);
+app.listen(5000, () => console.log('Started server on port 5000.'));
---
+++ routes/user.js
@@ -0,0 +1,92 @@
+const User = require('../models/user');
+const brcypt = require('bcryptjs');
+const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+router.post('/', (req, res) => {
+  const { email, passwordHash, passwordVerify } = req.body;
+  if (!email || !passwordHash || !passwordVerify) {
+    return res.status(400).json({
+      errorMessage: 'Please enter all required fields.',
+    });
+  }
+  if (passwordHash.length < 6) {
+    return res.status(400).json({
+      errorMessage: 'Password must be at least 6 characters long.',
+    });
+  }
+  if (passwordHash !== passwordVerify) {
+    return res.status(400).json({
+      errorMessage: 'Please enter same password twice.',
+    });
+  }
+
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      return res.status(400).json({
+        errorMessage: 'An account with this email already exists.',
+      });
+    } else {
+      brcypt.genSalt().then((salt) => {
+        brcypt.hash(passwordHash, salt).then((hashedPassword) => {
+          const newUser = new User({
+            email,
+            passwordHash: hashedPassword,
+          });
+          newUser.save().then(({ _id }) => {
+            const token = jwt.sign({ id: _id }, process.env.JWT_SECRET);
+            res.cookie('token', token, { httpOnly: true }).send();
+          });
+        });
+      });
+    }
+  });
+});
+
+router.post('/login', async (req, res) => {
+  const { email, passwordHash } = req.body;
+  if (!email || !passwordHash) {
+    return res.status(400).json({
+      errorMessage: 'Please enter all required fields.',
+    });
+  }
+
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) {
+    return res.status(401).json({
+      errorMessage: 'Wrong email or password.',
+    });
+  }
+  const correctPassword = await brcypt.compare(
+    passwordHash,
+    existingUser.passwordHash
+  );
+  if (!correctPassword)
+    return res.status(401).json({
+      errorMessage: 'Wrong email or password.',
+    });
+
+  const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET);
+  res.cookie('token', token, { httpOnly: true }).send();
+});
+
+router.get('/loggedIn', (req, res) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.json(null);
+    }
+    const validatedUser = jwt.verify(token, process.env.JWT_SECRET);
+    return res.json(validatedUser.id);
+  } catch {
+    return res.json(null);
+  }
+});
+
+router.get('/logout', (req, res) => {
+  try {
+    res.clearCookie('token').send();
+  } catch {
+    return res.json(null);
+  }
+});
+module.exports = router;

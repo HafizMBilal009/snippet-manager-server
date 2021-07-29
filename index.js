9d1f8c3b355eaf05e3289ef9b9703a00f5b3f322
HafizMBilal009 <bilal@datumbrain.com>
2021-07-29 12:55:35 +0500

    Changed CORS Origins

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://snippetmanager-datumbrain.netlify.app/',
+    ],
     credentials: true,
   })
 );

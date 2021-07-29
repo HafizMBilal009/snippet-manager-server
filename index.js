021d9b1406b3b16440060327c90131938f143bdc
HafizMBilal009 <bilal@datumbrain.com>
2021-07-29 13:31:07 +0500

    Updated Prod Url

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'https://snippetmanager-datumbrain.netlify.app/',
+      'https://snippetmanager-datumbrain.netlify.app',
     ],
     credentials: true,
   })
